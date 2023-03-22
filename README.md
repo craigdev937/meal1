c2face6746831f1bb78412647ff72fc3017b6858
Craig <indigomx9@gmail.com>
2023-03-21 20:06:56 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,9 @@
+Initial setup of the project.  Created with React, Webpack, TypeScript, Fetch API, The Meal DB API, and React Router.
+
+
+
+
+
+
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Alex from "@public/Alex Bars6.jpg";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <img 
+                src={Alex} alt="Alex Bars" 
+                height="600px" width="auto"
+            />
+        </React.Fragment>
+    );
+};
+
+
